f65248bd24eb2b8d6d8cd08d16cd8c02097bc3ef
John Hunt <john.a.hunt@aexp.com>
2022-02-05 14:04:54 -0700

    Cleanup in aisle EventJsonizer

--- graylog-provider/src/main/java/com/busybusy/graylog_provider/EventJsonizer.kt
+++ graylog-provider/src/main/java/com/busybusy/graylog_provider/EventJsonizer.kt
@@ -16,14 +16,6 @@
 package com.busybusy.graylog_provider
 
 import com.busybusy.analyticskit_android.AnalyticsEvent
-import java.lang.Error
-import java.lang.Exception
-import java.lang.StringBuilder
-import java.lang.UnsupportedOperationException
-import java.math.BigDecimal
-import java.math.BigInteger
-import java.util.HashMap
-import java.util.HashSet
 
 /**
  * Turns an [AnalyticsEvent] into a JSON String.
@@ -31,9 +23,8 @@ import java.util.HashSet
  * @author John Hunt on 6/29/17.
  */
 class EventJsonizer internal constructor(gelfSpecVersion: String, host: String) {
-    val HOST: String
-    val GELF_SPEC_VERSION: String
-    val MAX_FIELD_LENGTH = 32000
+    private val HOST: String
+    private val GELF_SPEC_VERSION: String
 
     init {
         GELF_SPEC_VERSION = getSafeSizeString(gelfSpecVersion)
@@ -46,177 +37,175 @@ class EventJsonizer internal constructor(gelfSpecVersion: String, host: String) 
      * @param event the event to serialize to JSON
      * @return the JSON string representation of the given event
      */
+    @Suppress("UNCHECKED_CAST")
+    @Throws(UnsupportedOperationException::class)
     fun getJsonBody(event: AnalyticsEvent): String {
-        val eventAttributes = if (event.attributes != null) event.attributes else HashMap()
-        val attributes: Set<String> =
-            if (event.attributes != null) event.attributes!!.keys else HashSet()
+        val eventAttributes = if (event.attributes != null) event.attributes else mutableMapOf()
+        val attributes: Set<String> = when {
+            event.attributes != null -> event.attributes!!.keys
+            else -> mutableSetOf()
+        }
 
-        // guard clause: Libraries SHOULD not allow to send id as additional field (_id).
+        // guard clause: Libraries SHOULD NOT allow to send id as additional field (_id).
         if (attributes.contains("id")) {
             throw UnsupportedOperationException("id is NOT allowed as an additional field according to the GELF spec!")
         }
-        val sb = StringBuilder("{")
-        sb.append("\"version\": \"").append(GELF_SPEC_VERSION).append("\", ")
-        sb.append("\"host\": \"").append(HOST).append("\", ")
-        sb.append("\"short_message\": \"").append(getSafeSizeString(event.name())).append("\", ")
-        if (attributes.contains("full_message")) {
-            val fullMessage = eventAttributes!!["full_message"].toString()
-            sb.append("\"full_message\": \"").append(getSafeSizeString(fullMessage)).append("\", ")
-        }
-        // No else needed: this is an optional long message (may contain a backtrace)
-        if (attributes.contains("timestamp")) {
-            val timestamp = eventAttributes!!["timestamp"].toString()
-            sb.append("\"timestamp\": ").append(getSafeSizeString(timestamp))
-                .append(", ") // user-provided timestamp
-        } else {
-            sb.append("\"timestamp\": ").append(System.currentTimeMillis() / 1000.0).append(", ")
-        }
-        if (attributes.contains("level")) {
-            val level = eventAttributes!!["level"].toString()
-            sb.append("\"level\": ").append(getSafeSizeString(level)) // user-provided syslog level
-        } else {
-            sb.append("\"level\": 6") // Informational
-        }
-        for (attribute in attributes) {
-            if (attribute.equals("version", ignoreCase = true) ||
-                attribute.equals("host", ignoreCase = true) ||
-                attribute.equals("short_message", ignoreCase = true) ||
-                attribute.equals("full_message", ignoreCase = true) ||
-                attribute.equals("timestamp", ignoreCase = true) ||
-                attribute.equals("level", ignoreCase = true)
-            ) {
-                // do nothing, these fields have already been handled above
-            } else {
-                val jsonAttribute = attribute.replace("\\s".toRegex(),
-                    "_") // Graylog omits fields whose name have white space, replace with '_'
-                sb.append(underscorePrefix(jsonAttribute))
-                val attributeValue = eventAttributes!![attribute]
-                if (attributeValue is String) // gotta use escape quotes for JSON strings
-                {
-                    sb.append(jsonAttribute).append("\": \"").append(getSafeSizeString(
-                        attributeValue)).append("\"")
-                } else if (attributeValue is List<*>) {
-                    sb.append(jsonAttribute).append("\": ").append(getJsonFromListRecursive(
-                        attributeValue))
-                } else if (attributeValue is Map<*, *>) {
-                    sb.append(jsonAttribute).append("\": ")
-                        .append(getJsonFromMapRecursive(attributeValue as Map<String, Any>))
-                } else if (isNumber(attributeValue) || attributeValue is Boolean) {
-                    sb.append(jsonAttribute).append("\": ").append(attributeValue)
-                } else if (attributeValue is Exception) {
-                    sb.append(jsonAttribute).append("\": \"").append(attributeValue.toString())
-                        .append("\"")
-                } else if (attributeValue is Error) {
-                    sb.append(jsonAttribute).append("\": \"").append(attributeValue.toString())
-                        .append("\"")
-                } else {
-                    throw UnsupportedOperationException("Unsupported type for GELF message: " + attributeValue!!.javaClass.simpleName)
+
+        return buildString {
+            append("{")
+            putGraylogSpecFields(this, event.name(), attributes, requireNotNull(eventAttributes))
+
+            for (attribute in attributes.filterNot {
+                // these fields have already been handled above
+                it.equals("version", ignoreCase = true) ||
+                        it.equals("host", ignoreCase = true) ||
+                        it.equals("short_message", ignoreCase = true) ||
+                        it.equals("full_message", ignoreCase = true) ||
+                        it.equals("timestamp", ignoreCase = true) ||
+                        it.equals("level", ignoreCase = true)
+            }) {
+                // Graylog omits fields whose name have white space, replace with '_'
+                val jsonAttribute = attribute.replace("\\s".toRegex(), "_")
+
+                append(", \"${underscorePrefix(jsonAttribute)}")
+                when (val attributeValue = eventAttributes[attribute]) {
+                    is String -> append(jsonAttribute).append("\": \"")
+                            .append(getSafeSizeString(attributeValue)).append("\"")
+                    is List<*> -> append(jsonAttribute).append("\": ").append(getJsonFromListRecursive(attributeValue))
+                    is Map<*, *> -> append(jsonAttribute).append("\": ")
+                            .append(getJsonFromMapRecursive(attributeValue as Map<String, Any>))
+                    is Number, is Boolean -> append(jsonAttribute).append("\": ").append(attributeValue)
+                    is Exception, is Error -> append(jsonAttribute).append("\": \"").append(attributeValue.toString())
+                            .append("\"")
+                    else -> {
+                        throw UnsupportedOperationException("Unsupported type for GELF message: " + attributeValue!!.javaClass.simpleName)
+                    }
                 }
             }
-        }
-        sb.append("}")
-        return sb.toString()
-    }
-
-    private fun underscorePrefix(fieldName: String): String {
-        return if (fieldName.startsWith("_")) {   // the user-provided field already has the underscore prefix, let's not prepend another underscore
-            ", \""
-        } else {   // the user didn't bother to prefix with underscores, let's do it for them to conform to the Graylog spec
-            ", \"_"
+            append("}")
         }
     }
 
-    fun getJsonFromMapRecursive(attributeMap: Map<String, Any>): String {
-        val json = StringBuilder("{")
+    private fun putGraylogSpecFields(
+            builder: StringBuilder,
+            eventName: String,
+            attributes: Set<String>,
+            eventAttributes: Map<String, Any>
+    ) = builder.apply {
+        append("\"version\": \"$GELF_SPEC_VERSION").append("\", ")
+        append("\"host\": \"$HOST").append("\", ")
+        append("\"short_message\": \"").append(getSafeSizeString(eventName)).append("\", ")
+
+        if (attributes.contains("full_message")) {
+            val fullMessage = eventAttributes["full_message"].toString()
+            append("\"full_message\": \"").append(getSafeSizeString(fullMessage)).append("\", ")
+        }
+        // No else needed: this is an optional long message (may contain a backtrace)
+
+        if (attributes.contains("timestamp")) { // user-provided timestamp
+            append("\"timestamp\": ").append(getSafeSizeString(eventAttributes["timestamp"].toString()))
+                    .append(", ")
+        } else {
+            append("\"timestamp\": ").append(System.currentTimeMillis() / 1000.0).append(", ")
+        }
+
+        if (attributes.contains("level")) { // user-provided syslog level
+            append("\"level\": ").append(getSafeSizeString(eventAttributes["level"].toString()))
+        } else {
+            append("\"level\": 6") // default to Informational syslog level
+        }
+    }
+
+    /**
+     * Ensures that user-provided fields conform to the Graylog spec (prefixed by an underscore)
+     */
+    private fun underscorePrefix(fieldName: String): String =
+            if (fieldName.startsWith("_")) "" else "_"
+
+    @Suppress("UNCHECKED_CAST")
+    internal fun getJsonFromMapRecursive(attributeMap: Map<String, Any>): String = buildString {
+        append("{")
         for (innerAttribute in attributeMap.keys) {
             val innerAttributeValue = attributeMap[innerAttribute]
             if (innerAttributeValue is Map<*, *>) // recursive case
             {
                 // recurse
                 val innerMap = attributeMap[innerAttribute] as Map<String, Any>?
-                json.append("\"").append(innerAttribute).append("\": ")
-                    .append(getJsonFromMapRecursive(
-                        innerMap!!)).append(", ")
+                append("\"").append(innerAttribute).append("\": ")
+                        .append(getJsonFromMapRecursive(
+                                innerMap!!)).append(", ")
             } else if (innerAttributeValue is List<*>) // recursive case
             {
-                json.append("\"").append(innerAttribute).append("\": ")
-                    .append(getJsonFromListRecursive(
-                        innerAttributeValue)).append(", ")
+                append("\"").append(innerAttribute).append("\": ")
+                        .append(getJsonFromListRecursive(
+                                innerAttributeValue)).append(", ")
             } else {
                 if (innerAttributeValue is String) // gotta use escape quotes for JSON strings
                 {
-                    json.append("\"").append(innerAttribute).append("\": \"")
-                        .append(getSafeSizeString(
-                            innerAttributeValue)).append("\", ")
-                } else if (isNumber(innerAttributeValue) || innerAttributeValue is Boolean) {
-                    json.append("\"").append(innerAttribute).append("\": ")
-                        .append(innerAttributeValue).append(", ")
+                    append("\"").append(innerAttribute).append("\": \"")
+                            .append(getSafeSizeString(
+                                    innerAttributeValue)).append("\", ")
+                } else if (innerAttributeValue is Number || innerAttributeValue is Boolean) {
+                    append("\"").append(innerAttribute).append("\": ")
+                            .append(innerAttributeValue).append(", ")
                 } else {
                     throw UnsupportedOperationException("Unsupported type for GELF message: " + innerAttributeValue!!.javaClass.simpleName)
                 }
             }
         }
-        if (json.toString().length > 1) {
-            json.deleteCharAt(json.toString().length - 1)
-            json.deleteCharAt(json.toString().length - 1)
+        if (this.toString().length > 1) {
+            deleteCharAt(this.toString().length - 1)
+            deleteCharAt(this.toString().length - 1)
         }
         // No else needed: the map was empty, no need to trim the last comma and space
-        json.append("}")
-        return json.toString()
+        append("}")
     }
 
-    fun getJsonFromListRecursive(attributeList: List<*>): String {
-        val json = StringBuilder("[")
+    @Suppress("UNCHECKED_CAST")
+    internal fun getJsonFromListRecursive(attributeList: List<*>): String = buildString {
+        append("[")
         for (element in attributeList) {
             if (element is Map<*, *>) // recursive case
             {
-                json.append(getJsonFromMapRecursive(element as Map<String, Any>)).append(", ")
+                append(getJsonFromMapRecursive(element as Map<String, Any>)).append(", ")
             } else if (element is List<*>) // recursive case
             {
-                json.append(getJsonFromListRecursive(element)).append(", ")
+                append(getJsonFromListRecursive(element)).append(", ")
             } else {
                 if (element is String) // gotta use escape quotes for JSON strings
                 {
-                    json.append("\"").append(getSafeSizeString(element)).append("\", ")
-                } else if (isNumber(element) || element is Boolean) {
-                    json.append(element).append(", ")
+                    append("\"").append(getSafeSizeString(element)).append("\", ")
+                } else if (element is Number || element is Boolean) {
+                    append(element).append(", ")
                 } else {
                     throw UnsupportedOperationException("Unsupported type for GELF message: " + element?.javaClass?.simpleName)
                 }
             }
         }
-        if (json.toString().length > 1) {
-            json.deleteCharAt(json.toString().length - 1)
-            json.deleteCharAt(json.toString().length - 1)
+        if (this.toString().length > 1) {
+            deleteCharAt(this.toString().length - 1)
+            deleteCharAt(this.toString().length - 1)
         }
         // No else needed: the list was empty, no need to trim the last comma and space
-        json.append("]")
-        return json.toString()
+        append("]")
     }
 
-    private fun getSafeSizeString(input: String): String {
-        return if (input.length <= MAX_FIELD_LENGTH) {
-            input
-        } else {
-            var safeSizeValue = input.substring(0, input.length.coerceAtMost(MAX_FIELD_LENGTH))
-            //correctly process UTF-16 surrogate pairs
-            if (safeSizeValue.length > MAX_FIELD_LENGTH) {
-                val correctedMaxWidth =
-                    if (Character.isLowSurrogate(safeSizeValue[MAX_FIELD_LENGTH])) MAX_FIELD_LENGTH - 1 else MAX_FIELD_LENGTH
-                safeSizeValue = safeSizeValue.substring(
-                    0,
-                    safeSizeValue.length.coerceAtMost(correctedMaxWidth)
-                )
+    private fun getSafeSizeString(input: String): String =
+            if (input.length <= MAX_FIELD_LENGTH) {
+                input
+            } else {
+                var safeSizeValue = input.substring(0, input.length.coerceAtMost(MAX_FIELD_LENGTH))
+                //correctly process UTF-16 surrogate pairs
+                if (safeSizeValue.length > MAX_FIELD_LENGTH) {
+                    val correctedMaxWidth =
+                            if (Character.isLowSurrogate(safeSizeValue[MAX_FIELD_LENGTH])) MAX_FIELD_LENGTH - 1 else MAX_FIELD_LENGTH
+                    safeSizeValue = safeSizeValue.substring(
+                            0,
+                            safeSizeValue.length.coerceAtMost(correctedMaxWidth)
+                    )
+                }
+                safeSizeValue
             }
-            safeSizeValue
-        }
-    }
-
-    private fun isNumber(attributeValue: Any?): Boolean {
-        return attributeValue is Int ||
-                attributeValue is Double ||
-                attributeValue is Long ||
-                attributeValue is BigInteger ||
-                attributeValue is BigDecimal
-    }
 }
+
+private const val MAX_FIELD_LENGTH = 32_000
--- graylog-provider/src/test/kotlin/com/busybusy/graylog_provider/EventJsonizerTest.kt
+++ graylog-provider/src/test/kotlin/com/busybusy/graylog_provider/EventJsonizerTest.kt
@@ -21,7 +21,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import java.util.*
 
 /**
  * Tests the [EventJsonizer] class.
@@ -45,10 +44,10 @@ class EventJsonizerTest {
                 .putAttribute("member_info", mapField)
         val now = System.currentTimeMillis() / 1000.0 // possibly flaky, shame on me
         val json = jsonizer.getJsonBody(event)
-        assertThat(json).isEqualTo("{\"version\": \"" + VERSION + "\", \"host\": \"" + HOST + "\", \"short_message\": \"" +
-                event.name() + "\", \"timestamp\": " + now + ", \"level\": 6, \"_test_attribute_1\": 100, " +
-                "\"_test_attribute_2\": \"200\", \"_member_info\": {\"first_name\": \"John\", \"last_name\": \"Jacob\", " +
-                "\"emails\": [\"john.jacob@unittest.me\", \"john.jacob@unittest.us\"]}}")
+        assertThat(json).isEqualTo("""{"version": "$VERSION", "host": "$HOST", "short_message": "${event.name()}",
+            | "timestamp": $now, "level": 6, "_test_attribute_1": 100, "_test_attribute_2": "200", "_member_info":
+            | {"first_name": "John", "last_name": "Jacob", "emails": ["john.jacob@unittest.me", "john.jacob@unittest.us"]}}"""
+                .trimMargin().replace("\n", ""))
     }
 
     @Test
@@ -64,11 +63,10 @@ class EventJsonizerTest {
                 .putAttribute("test_attribute_2", "200")
                 .putAttribute("member_info", mapField)
         val json = jsonizer.getJsonBody(event)
-        assertThat(json).isEqualTo("{\"version\": \"" + VERSION + "\", \"host\": \"" + HOST + "\", \"short_message\": \"" +
-                event.name() + "\", \"full_message\": \"This is a test of the JSON conversion\", " +
-                "\"timestamp\": " + timestamp + ", \"level\": 5, \"_test_attribute_1\": 100, " +
-                "\"_test_attribute_2\": \"200\", \"_member_info\": {\"first_name\": \"John\", \"last_name\": \"Jacob\", " +
-                "\"emails\": {\"personal\": \"john.jacob@unittest.me\", \"work\": \"john.jacob@unittest.us\"}}}")
+        assertThat(json).isEqualTo("""{"version": "$VERSION", "host": "$HOST", "short_message": "${event.name()}"
+            |, "full_message": "This is a test of the JSON conversion", "timestamp": $timestamp, "level": 5, "_test_attribute_1": 100,
+            | "_test_attribute_2": "200", "_member_info": {"first_name": "John", "last_name": "Jacob", "emails": {"personal":
+            | "john.jacob@unittest.me", "work": "john.jacob@unittest.us"}}}""".trimMargin().replace("\n", ""))
     }
 
     @Test
@@ -82,15 +80,15 @@ class EventJsonizerTest {
                 .putAttribute("test_attribute_2", "200")
                 .putAttribute("can_pass_booleans", true)
         val json = jsonizer.getJsonBody(event)
-        assertThat(json).isEqualTo("{\"version\": \"" + VERSION + "\", \"host\": \"" + HOST + "\", \"short_message\": \"" +
-                event.name() + "\", \"full_message\": \"This is a test of the JSON conversion\", " +
-                "\"timestamp\": " + timestamp + ", \"level\": 5, \"_test_attribute_1\": 100, " +
-                "\"_test_attribute_2\": \"200\", \"_can_pass_booleans\": true}")
+        assertThat(json).isEqualTo("""{"version": "$VERSION", "host": "$HOST", "short_message": "${event.name()}"
+            |, "full_message": "This is a test of the JSON conversion", "timestamp": $timestamp, 
+            |"level": 5, "_test_attribute_1": 100, "_test_attribute_2": "200", 
+            |"_can_pass_booleans": true}""".trimMargin().replace("\n", ""))
     }
 
     @Test
     fun jsonFromMapRecursive_emptyMap() {
-        val map: Map<String, Any> = HashMap()
+        val map = mutableMapOf<String, Any>()
         val json = jsonizer.getJsonFromMapRecursive(map)
         assertThat(json).isEqualTo("{}")
     }
@@ -99,7 +97,7 @@ class EventJsonizerTest {
     fun jsonFromMapRecursive_flatMapStructure() {
         val map = mutableMapOf<String, Any>("one" to "abc", "two" to 123, "three" to 321.123, "four" to true)
         val json = jsonizer.getJsonFromMapRecursive(map)
-        assertThat(json).isEqualTo("{\"one\": \"abc\", \"two\": 123, \"three\": 321.123, \"four\": true}")
+        assertThat(json).isEqualTo("""{"one": "abc", "two": 123, "three": 321.123, "four": true}""")
     }
 
     @Test
@@ -107,7 +105,7 @@ class EventJsonizerTest {
         val levelTwoMap = mutableMapOf("a value" to 123, "l3" to mutableMapOf<Any, Any>())
         val map = mutableMapOf<String, Any>("l2" to levelTwoMap)
         val json = jsonizer.getJsonFromMapRecursive(map)
-        assertThat(json).isEqualTo("{\"l2\": {\"a value\": 123, \"l3\": {}}}")
+        assertThat(json).isEqualTo("""{"l2": {"a value": 123, "l3": {}}}""")
     }
 
     @Test
@@ -115,7 +113,7 @@ class EventJsonizerTest {
         val levelTwoMap = mutableMapOf("a value" to 123, "l3" to mutableMapOf<Any, Any>())
         val map = mutableMapOf<String, Any>("l2" to levelTwoMap)
         val json = jsonizer.getJsonFromMapRecursive(map)
-        assertThat(json).isEqualTo("{\"l2\": {\"a value\": 123, \"l3\": {}}}")
+        assertThat(json).isEqualTo("""{"l2": {"a value": 123, "l3": {}}}""")
     }
 
     @Test
@@ -138,7 +136,7 @@ class EventJsonizerTest {
         val anotherMap = mutableMapOf<String, Any>("second" to "value", "second_num" to 31, "list_map_list" to listMapList)
         val list = mutableListOf<Any>(map, anotherMap)
         val json = jsonizer.getJsonFromListRecursive(list)
-        assertThat(json).isEqualTo("[{\"l2\": {\"a value\": 123, \"l3\": {}}}, {\"second\": \"value\", \"second_num\": 31, \"list_map_list\": " +
-                "[42, 24]}]")
+        assertThat(json).isEqualTo("""[{"l2": {"a value": 123, "l3": {}}}, {"second": "value", "second_num": 31, "list_map_list": [42, 24]}]""")
+
     }
 }
